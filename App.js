5ad9b0bb153eb25b09103d34c8b8bc3f6a30386d
Nik Rowe <NikRowe7@Gmail.com>
2020-04-24 10:50:48 -0700

    Components Part 2

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Main from './components/MainComponent'     
+import ErrorBoundary from './components/ErrorBoundryComponent';
 
 export default function App() {
   return (
-    <Main />
+    <ErrorBoundary>
+      <Main />
+    </ErrorBoundary>
   );
 }
 
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
+import { View } from 'react-native';
 import { CAMPSITES } from '../shared/campsites';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES
-        }
+            campsites: CAMPSITES,
+            selectedCampsite: null
+        };
+    }
+
+
+    onCampsiteSelect(campsiteId) {
+        this.setState({ selectedCampsite: campsiteId });
     }
 
     render() {
-        return <Directory campsites={this.state.campsites} />
+        return (
+            <View style={{ flex: 1, marginTop: 50 }}>
+                {/* This is what Nucamp instructions have - I added a Margin Top to bring the components down below the iphone X inset at the top 
+                    <View style={{flex: 1}}> */}
+                <Directory
+                    campsites={this.state.campsites}
+                    onPress={campsiteId => this.onCampsiteSelect(campsiteId)}
+                />
+                <CampsiteInfo
+                    campsite={this.state.campsites.filter(
+                        campsite => campsite.id === this.state.selectedCampsite)[0]}
+                />
+            </View>
+        )
     }
 }
 
