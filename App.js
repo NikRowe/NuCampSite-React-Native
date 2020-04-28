f1a76660cac3b12d8ed90ac3777b76026cac0c43
Nik Rowe <NikRowe7@Gmail.com>
2020-04-27 20:34:12 -0700

    Icons, Favorites, and Comments, no yellowbar warnings

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Main from './components/MainComponent'     
 import ErrorBoundary from './components/ErrorBoundryComponent';
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings(['Warning: ...'])
+console.disableYellowBox = true
 
 export default function App() {
   return (
